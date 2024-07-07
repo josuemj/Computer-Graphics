04739a857e5e14e07641a10d5a5b4d9b70e6d070
josuemj <josuemj456@gmail.com>
2024-07-06 23:02:51 -0600

    first commit

---
+++ gl.py
@@ -0,0 +1,23 @@
+#gl stands for graphics libraru
+
+class Render(object):
+    def __init__(self, screen):
+        self.screen = screen
+        _,_, self.width, self.height = screen.get_rect()
+        self.glColor(1,1,1)
+    
+    def glColor(self, r, g, b): #r, g, b on values from 0 to 1
+        r = min(1, max(0, r))
+        g = min(1, max(0, g))
+        b = min(1, max(0, b))
+
+        self.currentColor = [r,g,b]
+    
+    def glPoint(self, x, y, color = None):
+        #pygame renders from top top left corner
+        #but  BITMP starts from bottom left corner
+        #So we have to switch the 'y' value
+        if (0<=x<self.width) and (0<= y<self.height):
+            #pygame receices color into a range from 0 to 255
+            color = [int(i*255) for i in (color or self.currentColor)]
+            self.screen.set_at((x, self.height-1-y), color)
---
+++ mymath/linealalg.py
@@ -0,0 +1,8 @@
+
+def mult(matrix_1: list, matrix_2: list) -> list:
+    pass
+
+mult([],[])
+
+
+matrix = [[2,2,4],[2,2,4],[3,3,3],[4,4,4]]
