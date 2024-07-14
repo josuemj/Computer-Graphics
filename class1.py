193c2f45e48e872acca41a27f3547dfd10b0afb1
josuemj <josuemj456@gmail.com>
2024-07-14 10:46:36 -0600

    class 2

--- class1.py
+++ class1.py
@@ -5,11 +5,11 @@ from gl import Render
 width = 960
 height = 540 
 
-
-
 screen = pygame.display.set_mode(size=(width,height))
 clock = pygame.time.Clock() 
 rend = Render(screen)
+rend.glColor(1,0,1)
+rend.glClearColor(1, 0.5, 1)
 
 isRunning = True
 while isRunning:
@@ -20,7 +20,11 @@ while isRunning:
             if event.type == pygame.K_ESCAPE:
                 isRunning = False
     #rend stuff
-    rend.glPoint(45,500)
+
+    rend.glClear()
+    rend.glPoint(480,270)
+    for i in range(100):
+        rend.glPoint(480+i,270+i)
 
     pygame.display.flip()   
     clock.tick(60) # 60 frame per second
--- gl.py
+++ gl.py
@@ -5,6 +5,8 @@ class Render(object):
         self.screen = screen
         _,_, self.width, self.height = screen.get_rect()
         self.glColor(1,1,1)
+        self.glClearColor(0, 0, 0)
+        self.glClear()
     
     def glColor(self, r, g, b): #r, g, b on values from 0 to 1
         r = min(1, max(0, r))
@@ -12,6 +14,18 @@ class Render(object):
         b = min(1, max(0, b))
 
         self.currentColor = [r,g,b]
+    
+    def glClearColor(self, r, g, b):
+        r = min(1, max(0, r))
+        g = min(1, max(0, g))
+        b = min(1, max(0, b))
+
+        self.clearColor = [r,g,b]
+    
+    def glClear(self):
+        color = [int(i*255) for i in self.clearColor]
+        self.screen.fill(color)
+
     
     def glPoint(self, x, y, color = None):
         #pygame renders from top top left corner
@@ -21,3 +35,4 @@ class Render(object):
             #pygame receices color into a range from 0 to 255
             color = [int(i*255) for i in (color or self.currentColor)]
             self.screen.set_at((x, self.height-1-y), color)
+
