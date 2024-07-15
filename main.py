066d75b11c762dc5541b49368c0cf4d4adb640bf
josuemj <josuemj456@gmail.com>
2024-07-14 20:00:39 -0600

    filled polygons

--- main.py
+++ main.py
@@ -11,10 +11,19 @@ rend = Render(screen)
 
 pol_1 = [(165, 380) ,(185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]
 pol_2 = [(321, 335), (288, 286), (339, 251), (374, 302)]
+pol_3 = [(377, 249), (411, 197), (436, 249)]
+pol_4 = [(413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52),
+(750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230),
+(597, 215), (552, 214), (517, 144), (466, 180)
+]
+
+pol_5 = [(682, 175), (708, 120), (735, 148), (739, 170)
+]
 
 def poligono(points):
     for i in range(len(points)):
         rend.glLine(points[i], points[(i+1) % len(points)])
+
 
 isRunning = True
 while isRunning:
@@ -25,16 +34,32 @@ while isRunning:
             if event.type == pygame.K_ESCAPE:
                 isRunning = False
     #rend stuff
-
     rend.glClear()
-
-    rend.glColor(0.3,0.3,0.3)
+    
+    #star
+    rend.glColor(0,1,1)
     poligono(pol_1)
     rend.glFillPolygon(pol_1)  
-
-    rend.glColor(0.9,0.3,0.3)
+    
+    #quad
+    rend.glColor(0.125,1,0)
     poligono(pol_2)
     rend.glFillPolygon(pol_2) 
+
+    #Triangle
+    rend.glColor(0.9,0.3,0.3)
+    poligono(pol_3)
+    rend.glFillPolygon(pol_3)
+
+    #lamp
+    rend.glColor(1,1,0)
+    poligono(pol_4)
+    rend.glFillPolygon(pol_4)
+
+    #whole inside lamp
+    rend.glColor(0, 0, 0)
+    poligono(pol_5)
+    rend.glFillPolygon(pol_5)
     
     
 
