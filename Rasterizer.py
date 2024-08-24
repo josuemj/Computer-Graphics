c0bfe60003f65660bb6bdc6512c87bb56b4bcf38
josuemj <98498772+josuemj@users.noreply.github.com>
2024-08-23 21:58:42 -0600

    added lion

--- Rasterizer.py
+++ Rasterizer.py
@@ -5,49 +5,49 @@ from gl import *
 from model import Model
 from shaders import *
 
-width = 940
-height = 540
+width = 1792
+height = 1024
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED  )
 clock = pygame.time.Clock()
 
 rend = Renderer(screen)
-rend.glLoadBackground('textures/fondo.bmp')
+rend.glLoadBackground('textures/garden.bmp')
 
-modelo1 = Model('models/base.obj')
-modelo1.loadTexture('textures/hearth.bmp')
+modelo1 = Model('models/LionSculpture.obj')
+modelo1.loadTexture('textures/ModelTexture.bmp')
 modelo1.vertexShader = vertexShader
-modelo1.fragmentShader = greenShadow
+modelo1.fragmentShader = fragmentShader
 modelo1.translate[2] = -5
 modelo1.translate[0] = -2
-modelo1.scale[0] = 1.5
-modelo1.scale[1] = 1.5 
-modelo1.scale[2] = 1.5
+modelo1.scale[0] = 2
+modelo1.scale[1] = 2 
+modelo1.scale[2] = 2
 
-modelo2 = Model('models/base.obj')
-modelo2.loadTexture('textures/hearth.bmp')
-modelo2.vertexShader = vertexShader
-modelo2.fragmentShader = fragmentShader
-modelo2.translate[2] = -5
-modelo2.translate[0] = 0
-modelo2.scale[0] = 1.5
-modelo2.scale[1] = 1.5
-modelo2.scale[2] = 1.5
+# modelo2 = Model('models/base.obj')
+# modelo2.loadTexture('textures/hearth.bmp')
+# modelo2.vertexShader = vertexShader
+# modelo2.fragmentShader = fragmentShader
+# modelo2.translate[2] = -5
+# modelo2.translate[0] = 0
+# modelo2.scale[0] = 1.5
+# modelo2.scale[1] = 1.5
+# modelo2.scale[2] = 1.5
 
-modelo3 = Model('models/base.obj')
-modelo3.loadTexture('textures/hearth.bmp')
-modelo3.vertexShader = vertexShader
-modelo3.fragmentShader = checkerShader
-modelo3.translate[2] = -5
-modelo3.translate[0] = 2
-modelo3.scale[0] = 1.5
-modelo3.scale[1] = 1.5
-modelo3.scale[2] = 1.5
+# modelo3 = Model('models/base.obj')
+# modelo3.loadTexture('textures/hearth.bmp')
+# modelo3.vertexShader = vertexShader
+# modelo3.fragmentShader = checkerShader
+# modelo3.translate[2] = -5
+# modelo3.translate[0] = 2
+# modelo3.scale[0] = 1.5
+# modelo3.scale[1] = 1.5
+# modelo3.scale[2] = 1.5
 
 
 rend.models.append(modelo1)
-rend.models.append(modelo2)
-rend.models.append(modelo3)
+# rend.models.append(modelo2)
+# rend.models.append(modelo3)
 
 
 isRunning = True
@@ -69,11 +69,11 @@ while isRunning:
 				rend.primitiveType = TRIANGLES
 			
 			elif event.key == pygame.K_4: #roll (x)
-				modelo2.rotate[0] += 5			
+				modelo1.rotate[0] += 5			
 			elif event.key == pygame.K_5:# Yaw (y)
-				modelo2.rotate[1] += 5
+				modelo1.rotate[1] += 5
 			elif event.key == pygame.K_6:# pitch (z)
-				modelo2.rotate[2] += 5
+				modelo1.rotate[2] += 5
 				
 			elif event.key == pygame.K_RIGHT:
 				rend.camera.translate[0] += 1
