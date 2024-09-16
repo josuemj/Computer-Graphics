8cc59a71246f33836028a371e95e7229ad5b4450
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-16 17:54:02 -0600

    feat!: Snowman shaped (3 basis spheres)

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -8,8 +8,8 @@ from material import Material
 from lights import *
 
 
-width = 256
-height = 256
+width =  512
+height = 512
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED )
 clock = pygame.time.Clock()
@@ -18,14 +18,23 @@ rt =  RendererRT(screen)
 
 brick = Material(difuse=[1, 0.2, 0.2], spec=128, Ks=0.25)
 grass = Material(difuse=[0.2, 1.0, 0.2], spec=64, Ks=0.2)
-
+snowy = Material(
+    difuse=[0.95, 0.95, 0.95],
+    spec=128,
+    Ks=0.3
+)
 
 rt.lights.append( DirectionalLight(direction = [-1, -1, -1], intensity = 0.8) )
 rt.lights.append( DirectionalLight(direction = [0.5, -0.5, -1], intensity = 0.8, color = [1,1,1] ))
 rt.lights.append( AmbientLight(intensity=0.1))
 
-rt.scene.append( Sphere(position = [0, 0 , -5], radius = 1.5, material = brick) )
-rt.scene.append( Sphere(position = [1, 1 , -3], radius = 0.5, material = grass) )
+# rt.scene.append( Sphere(position = [0, 0 , -5], radius = 1.5, material = brick) )
+# rt.scene.append( Sphere(position = [1, 1 , -3], radius = 0.5, material = grass) )
+
+rt.scene.append( Sphere(position=[0, 0.6, -2.8], radius=0.3, material=snowy))
+rt.scene.append( Sphere(position=[0, 0, -2.9], radius=0.4, material=snowy))
+rt.scene.append( Sphere(position=[0, -0.7, -3], radius=0.5, material=snowy))
+
 
 
 rt.glRender()
