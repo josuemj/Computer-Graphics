77d1e4a236516bd222b844169db455a9b7e35e12
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-16 22:15:58 -0600

    removing numpy lights.py

--- lights.py
+++ lights.py
@@ -1,6 +1,5 @@
-import numpy as np
-from MathLib import reflectVector
-
+from MathLib import *
+    
 class Light(object):
     def __init__(self, color = [1, 1, 1], intensity = 1.0, lighType = "None"):
         self.color = color
@@ -20,13 +19,13 @@ class AmbientLight(Light):
 class DirectionalLight(Light):
     def __init__(self, color = [1,1,1], intensity = 1.0, direction = [0, -1, 0 ]):
         super().__init__(color, intensity, "Directional")
-        self.direction = direction / np.linalg.norm(direction)
+        self.direction = normalize_vector(direction)
     
     def GetLightColor(self, intercept = None):
         lightColor =  super().GetLightColor()
         if intercept:
             dir = [(i * -1) for i in self.direction]
-            intensity = np.dot(intercept.normal, dir)
+            intensity = dot(intercept.normal, dir)
             intensity = max(0, min(1, intensity))
             intensity *= (1 - intercept.obj.material.Ks)
             lightColor = [(i * intensity) for i in lightColor]
@@ -39,10 +38,10 @@ class DirectionalLight(Light):
             dir = [(i * -1) for i in self.direction]
             reflect = reflectVector(intercept.normal, dir)
             
-            viewDir = np.subtract(viewPos, intercept.point)
-            viewDir /= np.linalg.norm(viewDir)
+            viewDir = restar_elementos(viewPos, intercept.point)
+            viewDir = normalize_vector(viewDir)
             
-            specularity = max(0, np.dot(viewDir, reflect) ** intercept.obj.material.spec)
+            specularity = max(0, dot(viewDir, reflect) ** intercept.obj.material.spec)
             specularity *= intercept.obj.material.Ks
             specularity *= self.intensity
             specColor = [(i * specularity) for i in specColor]
