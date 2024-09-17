0cf47762e55a2969a9ea473646a18d82f18508b0
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-16 18:26:25 -0600

    feat: bottom button positiones and material

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -18,10 +18,17 @@ rt =  RendererRT(screen)
 
 brick = Material(difuse=[1, 0.2, 0.2], spec=128, Ks=0.25)
 grass = Material(difuse=[0.2, 1.0, 0.2], spec=64, Ks=0.2)
+
 snowy = Material(
     difuse=[0.95, 0.95, 0.95],
     spec=128,
     Ks=0.3
+)
+
+button = Material(
+    difuse=[0.09, 0.09, 0.09],  # Very dark grey, nearly black
+    spec=32,                  # Lower specular exponent for a softer highlight
+    Ks=0.1                        # Low specular coefficient for a matte finish
 )
 
 rt.lights.append( DirectionalLight(direction = [-1, -1, -1], intensity = 0.8) )
@@ -32,8 +39,11 @@ rt.lights.append( AmbientLight(intensity=0.1))
 # rt.scene.append( Sphere(position = [1, 1 , -3], radius = 0.5, material = grass) )
 
 rt.scene.append( Sphere(position=[0, 0.6, -2.8], radius=0.3, material=snowy))
+
 rt.scene.append( Sphere(position=[0, 0, -2.9], radius=0.4, material=snowy))
+
 rt.scene.append( Sphere(position=[0, -0.7, -3], radius=0.5, material=snowy))
+rt.scene.append( Sphere(position=[0, -0.5, -2.6], radius=0.09, material=button))
 
 
 
