362920f1af1731c73f2fb184c48c0b6b79ebd97e
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-28 23:20:11 -0600

    new brach for rt3 lab structured

--- model.py
+++ model.py
@@ -4,29 +4,39 @@ from texture import Texture
 
 
 class Model(object):
-	def __init__(self, filename):
-		objFile = Obj(filename)
-		
-		self.vertices = objFile.vertices
-		self.faces = objFile.faces
-		self.textCoords = objFile.texcoords # new one 
-		self.normals = objFile.normals
-		
-		self.translate = [0,0,0]
-		self.rotate = [0,0,0]
-		self.scale = [1,1,1]
+    def __init__(self, filename):
+        objFile = Obj(filename)
+        
+        self.vertices = objFile.vertices
+        self.texCoords = objFile.texcoords
+        self.normals = objFile.normals
+        self.faces = objFile.faces
+        
+        self.translate = [0,0,0]
+        self.rotate = [0,0,0]
+        self.scale = [1,1,1]
+        
+        self.texture = None
+        
+        self.vertexShader = None
+        self.fragmentShader = None
+        
 
-		self.texCoords = objFile.texcoords
-		self.texture = None
+    def GetModelMatrix(self):
+        translateMat = TranslationMatrix(self.translate[0],
+                                         self.translate[1],
+                                         self.translate[2])
+        
+        rotateMat = RotationMatrix(self.rotate[0],
+                                   self.rotate[1],
+                                   self.rotate[2])
+        
+        scaleMat = ScaleMatrix(self.scale[0],
+                               self.scale[1],
+                               self.scale[2]) 
+        
+        return translateMat * rotateMat * scaleMat
+    
 
-		self.vertexShader = None
-		self.fragmentShader = None
-
-	def loadTexture(self, filename):
-		self.texture = Texture(filename)
-		
-	def GetModelMatrix(self):
-		translateMat = TranslationMatrix(self.translate[0], self.translate[1], self.translate[2])
-		rotateMat = RotationMatrix(self.rotate[0], self.rotate[1], self.rotate[2])
-		scaleMat = ScaleMatrix(self.scale[0], self.scale[1], self.scale[2])
-		return matrix_multiply(matrix_multiply(translateMat, rotateMat), scaleMat)
+    def LoadTexture(self, filename):
+        self.texture = Texture(filename)
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-![output](output.bmp)
+## red plane + mirror sphere
+![red plane + mirror sphere](outputs/output1.png)
+
+## disk mirror + red sphere
+![disk mirror + red sphere](outputs/output2.png)
+
+## 3 AABB cubes
+![3 AABB cubes](outputs/output3.png)
+
+## 4 AABB cubes + textured wood
+![4 AABB cubes](outputs/output4.png)
