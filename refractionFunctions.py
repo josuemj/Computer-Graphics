7d438f7ef64efd4fe35857a229f07e42b3e88c35
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-29 22:04:36 -0600

    feat -> added mathLib

--- refractionFunctions.py
+++ refractionFunctions.py
@@ -1,56 +1,64 @@
-import numpy as np
 from math import acos, asin, pi
+from MathLib import *
 
 def refractVector(normal, incident, n1, n2):
-    # Ensure normal and incident are numpy arrays
-    normal = np.array(normal)
-    incident = np.array(incident)
-
-    # Snell's Law
-    c1 = np.dot(normal, incident)
+    # Snell's Law		
+    c1 = dotP(normal, incident)
     
     if c1 < 0:
         c1 = -c1
     else:
-        normal = np.array(normal) * -1
+        normal = scalar_multiply(normal, -1)
         n1, n2 = n2, n1
 
     n = n1 / n2
 
-    T = n * (incident + c1 * normal) - normal * (1 - n**2 * (1 - c1**2 )) ** 0.5
+    # Multiplicación de vectores y escalares sin numpy
+    temp_vec = scalar_multiply(normal, c1)
+    incident_plus_normal = add(incident, temp_vec)
 
-    return T / np.linalg.norm(T)
+    T1 = scalar_multiply(incident_plus_normal, n)
+    
+    factor = (1 - n**2 * (1 - c1**2)) ** 0.5
+    T2 = scalar_multiply(normal, factor)
+
+    T = substraction(T1, T2)
+
+    # Normalizamos el vector T sin usar numpy
+    return normalize(T)
 
 
 def totalInternalReflection(normal, incident, n1, n2):
-	c1 = np.dot(normal, incident)
-	if c1 < 0:
-		c1 = -c1
-	else:
-		n1, n2 = n2, n1
-		
-	if n1 < n2:
-		return False
-	
-	theta1 = acos(c1)
-	thetaC = asin(n2/n1)
-	
-	return theta1 >= thetaC
+    c1 = dotP(normal, incident)
+    
+    if c1 < 0:
+        c1 = -c1
+    else:
+        n1, n2 = n2, n1
+
+    if n1 < n2:
+        return False
+
+    theta1 = acos(c1)
+    thetaC = asin(n2 / n1)
+
+    return theta1 >= thetaC
 
 
 def fresnel(normal, incident, n1, n2):
-	c1 = np.dot(normal, incident)
-	if c1 < 0:
-		c1 = -c1
-	else:
-		n1, n2 = n2, n1
+    c1 = dotP(normal, incident)
+    
+    if c1 < 0:
+        c1 = -c1
+    else:
+        n1, n2 = n2, n1
 
-	s2 = (n1 * (1 - c1**2)**0.5) / n2
-	c2 = (1 - s2 ** 2) ** 0.5
-	
-	F1 = (((n2 * c1) - (n1 * c2)) / ((n2 * c1) + (n1 * c2))) ** 2
-	F2 = (((n1 * c2) - (n2 * c1)) / ((n1 * c2) + (n2 * c1))) ** 2
+    s2 = (n1 * (1 - c1**2) ** 0.5) / n2
+    c2 = (1 - s2 ** 2) ** 0.5
 
-	Kr = (F1 + F2) / 2
-	Kt = 1 - Kr
-	return Kr, Kt
+    F1 = (((n2 * c1) - (n1 * c2)) / ((n2 * c1) + (n1 * c2))) ** 2
+    F2 = (((n1 * c2) - (n2 * c1)) / ((n1 * c2) + (n2 * c1))) ** 2
+
+    Kr = (F1 + F2) / 2
+    Kt = 1 - Kr
+    return Kr, Kt
