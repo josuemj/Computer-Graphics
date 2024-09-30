32b7b9e650bb39ff38812d43d46f05259ea1a727
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-29 21:52:30 -0600

    feta -> added tnt.bmp texture + lights adjusted

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -8,8 +8,8 @@ from material import *
 from lights import *
 from texture import Texture
 
-width = 320
-height = 240
+width = 640
+height = 480
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED )
 clock = pygame.time.Clock()
@@ -27,63 +27,40 @@ bluemirror = Material(difuse=[0.5, 0.5, 1], spec = 128, Ks = 0.2, matType = REFL
 earth = Material(texture=Texture('textures/earth.bmp'))
 marble = Material(texture=Texture('textures/whiteMarble.bmp'), spec=128, Ks = 0.2, matType=REFLECTIVE)
 glass = Material(ior = 1.5, spec = 128, Ks = 0.2, matType=TRANSPARENT)
-
 woodenBox = Material( texture=Texture("textures/woodenBox.bmp"))
+tnt = Material( texture=Texture("textures/tnt.bmp"))
 
-# rt.lights.append( DirectionalLight(direction = [-1, -1, -1], intensity = 0.8) )
-# # rt.lights.append( DirectionalLight(direction = [0.5, -0.5, -1], intensity = 0.8, color = [1,1,1] ))
-# rt.lights.append( AmbientLight(intensity=0.1))
-
-# rt.lights = []  # Reinicia las luces
 rt.lights.append(DirectionalLight(direction=[1, -1, -1], intensity=1))  # Luz direccional más fuerte y en otro ángulo
-rt.lights.append(AmbientLight(intensity=0.9))  # Aumenta la luz ambiental
-# rt.lights.append(DirectionalLight(direction=[0.5, -0.5, -1], intensity=0.5))  
+rt.lights.append(AmbientLight(intensity=1))  # Aumenta la luz ambiental
 
-
-# rt.scene.append( Sphere(position = [0, 0 , -5], radius = 1, material = brick) )
-# rt.scene.append( Sphere(position = [1, 1 , -3], radius = 0.5, material = earth) )
-
-#planes
-# rt.scene.append( Plane(position=[0,-5,-5], normal= [0,1,0], material=brick))
-
-#disk
-# rt.scene.append( Disk(position=[0, -1, -5], normal = [0, 1, 0], radius=1.5, material=mirror))
-
-#cubes
-# rt.scene.append( AABB(position = [1.5,1.5,-5], sizes = [1,1,1], material = brick))
-# rt.scene.append( AABB(position = [-1.5,1.5,-5], sizes = [1,1,1], material = mirror))
-# rt.scene.append( AABB(position = [1.5,-1.5,-5], sizes = [1,1,1], material = woodenBox))
-# rt.scene.append( AABB(position = [-1.5,-1.5,-5], sizes = [1,1,1], material = glass))
 
 #wall materials
-# Definir materiales con colores más claros
 white_floor_material = Material(difuse=[1.0, 1.0, 1.0], spec=1.5, Ks=0.1, )  # Blanco más brillante
 red_material = Material(difuse=[1.0, 0.5, 0.5], spec=1.5, Ks=0.1, )  # Rojo claro
 green_material = Material(difuse=[0.5, 1.0, 0.5], spec=1.5, Ks=0.1,  )  # Verde claro
 blue_material = Material(difuse=[0.5, 0.5, 1.0], spec=1.5, Ks=0.1,  )  # Azul claro
 yellow_material = Material(difuse=[1.0, 1.0, 0.5], spec=1.5, Ks=0.1  )  # Amarillo claro
 cyan_material = Material(difuse=[0.5, 1.0, 1.0], spec=1.5, Ks=0.1)  # Cian claro
+blue_dark_sea_material = Material(difuse=[0.0, 0.2, 0.5], spec=1.5, Ks=0.3)  
+brown_material = Material(difuse=[0.4, 0.2, 0.1], spec=1.5, Ks=0.3)  
+mustard_yellow_material = Material(difuse=[0.8, 0.7, 0.2], spec=1.5, Ks=0.3)  
+light_lime_green_material = Material(difuse=[0.6, 1.0, 0.4], spec=1.5, Ks=0.3)  
 
+#planes
 floor = Plane(position=[0, -2, 0], normal=[0, 1, 0], material=white_floor_material)
+right_wall = Plane(position=[4, 0, 0], normal=[-1, 0, 0], material=blue_dark_sea_material)
+left_wall = Plane(position=[-4, 0, 0], normal=[1, 0, 0], material=light_lime_green_material )
+back_wall = Plane(position=[0, 0, -10], normal=[0, 0, 1], material=mustard_yellow_material)
+ceiling = Plane(position=[0, 4, 0], normal=[0, -1, 0], material=brown_material)
 
-
-right_wall = Plane(position=[4, 0, 0], normal=[-1, 0, 0], material=brick)
-
-# Pared Izquierda: Normal apuntando hacia la derecha (1 en el eje X)
-left_wall = Plane(position=[-4, 0, 0], normal=[1, 0, 0], material=green_material)
-
-# Pared del Fondo: Normal apuntando hacia la cámara o hacia adelante (1 en el eje Z)
-back_wall = Plane(position=[0, 0, -10], normal=[0, 0, 1], material=blue_material)
-ceiling = Plane(position=[0, 4, 0], normal=[0, -1, 0], material=cyan_material)
-
-# Añadimos las paredes a la escena
+#scence addition
 rt.scene.append(right_wall)
 rt.scene.append(left_wall)
 rt.scene.append(back_wall)
 rt.scene.append(floor)
 rt.scene.append(ceiling)
 rt.scene.append( Disk(position=[1.5, -1.9, -7], normal = [0, 1, 0], radius=1.5, material=mirror))
-rt.scene.append( AABB(position = [-1.2,-1.1,-4], sizes = [1,1,1], material = brick))
+rt.scene.append( AABB(position = [-1.2,-1.1,-4], sizes = [1,1,1], material = tnt))
 rt.scene.append( AABB(position = [1.5,-1.5,-7], sizes = [1,1,1], material = woodenBox))
 
 
